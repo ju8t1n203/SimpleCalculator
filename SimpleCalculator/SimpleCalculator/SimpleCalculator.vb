404c8b8f23db3f9ddb005f1bff3c6abed2f37db9
Juxin <Juxin@JUSITIN>
2024-09-01 14:09:57 -0600

    Add project files.

---
+++ SimpleCalculator/SimpleCalculator/SimpleCalculator.vb
@@ -0,0 +1,43 @@
+﻿'Justin Bell
+'RCET0256
+'Fall2024
+'SimpleCalculator
+'
+Option Explicit On
+Option Strict On
+Module SimpleCalculator
+
+    Sub Main()
+
+        'specifing variables
+        Dim firstnumber As Integer
+        Dim secondnumber As Integer
+        Dim userinput As String
+        Dim [select] As String
+
+        'where the user inputs the two numbers they wish to manipulate
+        Console.WriteLine("Enter a Whole Number:")
+        userinput = Console.ReadLine()
+        firstnumber = CInt(userinput)
+        Console.WriteLine("Enter Second Whole Number:")
+        userinput = Console.ReadLine()
+        secondnumber = CInt(userinput)
+
+        'user selects mathmatical operation
+        Console.WriteLine("Please Select Opteration:")
+        Console.WriteLine("1. Addition")
+        Console.WriteLine("2. Multiplication")
+        [select] = Console.ReadLine()
+
+        'mathmatical operation is carried out
+        If [select] = "1" Then
+            Console.WriteLine($"{firstnumber} + {secondnumber} = {firstnumber + secondnumber}")
+        ElseIf [select] = "2" Then
+            Console.WriteLine($"{firstnumber} * {secondnumber} = {firstnumber * secondnumber}")
+        End If
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
